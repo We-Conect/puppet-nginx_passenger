a602d32537a5ff80468b6498ce28645ccd1809a7
Nick Thomas <nick.thomas@mobileeventguide.de>
2013-11-12 12:50:38 +0100

    Update install.pp
    removing definition of sources.list.d because it's already handled by puppetlabs-apt

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,12 +8,6 @@ class nginx_passenger::install {
     require => Exec['apt-get update'],
   }
 
-  file { '/etc/apt/sources.list.d':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => 0755,
-  }
 
   file { '/etc/apt/sources.list.d/nginx_passenger.list':
     content => "deb https://oss-binaries.phusionpassenger.com/apt/passenger ${::lsbdistcodename} main\n",
@@ -21,7 +15,6 @@ class nginx_passenger::install {
     owner   => 'root',
     group   => 'root',
     mode    => 0644,
-    require => File['/etc/apt/sources.list.d'],
   }
 
   exec { 'apt-get update':
